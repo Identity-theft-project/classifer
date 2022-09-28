6abf6206797df2de551e613be69fe7983f7370ad
Karan Suryadevra <karansuryadevra@outlook.com>
2022-09-27 23:46:42 -0400

    Making progress

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 #! /home/karansuryadevra/Dev/ebcs/classifier/bin/python3
 
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import cv2
+import random
+import pickle
 import tensorflow as tf 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
@@ -8,9 +14,64 @@ from keras.models import model_from_json
 from keras.models import load_model
 import matplotlib.pyplot as plt
 
-# Opening the files about data
-X = pickle.load(open("X.pickle", "rb"))
-y = pickle.load(open("y.pickle", "rb"))
+
+file_list = []
+class_list = []
+
+DATADIR = "data"
+
+# All the categories you want your neural network to detect
+CATEGORIES = ["check", "license"]
+
+# The size of the images that your neural network will use
+IMG_SIZE = 50
+
+# Checking or all images in the data folder
+for category in CATEGORIES :
+	path = os.path.join(DATADIR, category)
+	for img in os.listdir(path):
+		img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+
+training_data = []
+
+def create_training_data():
+	for category in CATEGORIES :
+		path = os.path.join(DATADIR, category)
+		class_num = CATEGORIES.index(category)
+		for img in os.listdir(path):
+			try :
+				img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+				new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+				training_data.append([new_array, class_num])
+			except Exception as e:
+				pass
+
+create_training_data()
+
+random.shuffle(training_data)
+
+X = [] #features
+y = [] #labels
+
+for features, label in training_data:
+	X.append(features)
+	y.append(label)
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+y = np.array(y)
+print(X.shape)
+print(y.shape)
+# # Creating the files containing all the information about your model
+# pickle_out = open("X.pickle", "wb")
+# pickle.dump(X, pickle_out)
+# pickle_out.close()
+
+# pickle_out = open("y.pickle", "wb")
+# pickle.dump(y, pickle_out)
+# pickle_out.close()
+
+# pickle_in = open("X.pickle", "rb")
+# X = pickle.load(pickle_in)
 
 # normalizing data (a pixel goes from 0 to 255)
 X = X/255.0
@@ -65,9 +126,11 @@ model.save('CNN.model')
 # Printing a graph showing the accuracy changes during the training phase
 print(history.history.keys())
 plt.figure(1)
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'validation'], loc='upper left')
+
+
